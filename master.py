f632f058a4873ec63aa1932e1a540a4a7a40efdd
ShockedPlot7560 <no-reply@tchallon.fr>
2025-05-23 09:23:37 +0200

    add postgres

--- master.py
+++ master.py
@@ -1,16 +1,42 @@
+from traci import switch
+
+
 def main(host, port):
     import paho.mqtt.client as mqtt
-    import logging
+    import logging, json, psycopg2
     logger = logging.getLogger(__name__)
 
-    topic = "demo/topic"
+    controller_lane_topic = "controller/lane"
+    controller_traffic_light_topic = "controller/traffic_light"
+
+    logging.debug("Connecting to database...")
+    db = psycopg2.connect(
+        dbname="claxon",
+        user="user",
+        password="password",
+        host="localhost"
+    )
 
     def on_connect(client, userdata, flags, rc):
         logger.info("Connected with result code "+str(rc))
-        client.subscribe(topic)
+        client.subscribe(controller_lane_topic)
+        client.subscribe(controller_traffic_light_topic)
 
     def on_message(client, userdata, msg):
-        logger.debug(f"Message reçu: {msg.payload.decode()} sur le topic {msg.topic}")
+        if msg.topic == controller_lane_topic:
+            logger.info("Received lane update")
+            try:
+                data = json.loads(msg.payload.decode())
+            except Exception as e:
+                logger.error("Failed to process traffic light update: " + str(e))
+        elif msg.topic == controller_traffic_light_topic:
+            logger.info("Received traffic light update")
+            try:
+                data = json.loads(msg.payload.decode())
+            except Exception as e:
+                logger.error("Failed to process traffic light update: " + str(e))
+        else:
+            logger.error("Topic inconnu: " + msg.topic)
 
     client = mqtt.Client(client_id="master", clean_session=False)
     client.enable_logger(logger)
@@ -18,4 +44,7 @@ def main(host, port):
     client.on_message = on_message
 
     client.connect(host, port, 60)
-    client.loop_forever()
+    try:
+        client.loop_forever()
+    except KeyboardInterrupt:
+        logger.info("Shutting down")
